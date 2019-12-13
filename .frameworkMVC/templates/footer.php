466e4035f5e62b69cc5cdc67fe6efa751b87ec7a
Warrior62 <50228030+Warrior62@users.noreply.github.com>
2019-12-14 00:27:10 +0100

    Make the footer fixed on the bottom
    position: fixed;
    bottom: 0;

--- .frameworkMVC/templates/footer.php
+++ .frameworkMVC/templates/footer.php
@@ -15,6 +15,13 @@ if (basename($_SERVER["PHP_SELF"]) != "index.php")
         <meta charset='UTF-8'>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">    
     </head>
+    <style>
+      footer{
+	position: fixed;
+	bottom: 0;
+	width: 100%;
+      }
+    </style>
 
 
 <footer class="page-footer pt-5 text-center">
